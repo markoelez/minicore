c6835cde06bce6240769e45ff56f09b09f1194e8
Marko Elez <markoelez7@gmail.com>
2022-05-23 22:15:06 -0700

    add test passes

--- core.py
+++ core.py
@@ -7,6 +7,12 @@ from elftools.elf.elffile import ELFFile
 DRAM_SIZE = 64 * (2 ** 10)  # 16 Kb
 DRAM_BASE = 0x80000000
 
+regnames = \
+    ['x0', 'ra', 'sp', 'gp', 'tp'] + [f't{x}' for x in range(3)] + ['s0', 's1'] +\
+    [f'a{x}' for x in range(8)] + \
+    [f's{x}' for x in range(2, 12)] + \
+    [f't{x}' for x in range(3, 7)] + ["PC"]
+
 class Ops(Enum):
     LUI = 0b0110111 # load upper immediate
     LOAD = 0b0000011
@@ -102,7 +108,7 @@ class REGFILE:
             if i != 0 and i % 8 == 0 or i == len(self.regs) - 1:
                 res.append('\t'.join(lvl))
                 lvl = []
-            lvl.append(f'x{i:<2}: {r:08x}')
+            lvl.append(f'{regnames[i]:<2}: {r:08x}')
         res.append(f'PC : {self.regs[self.PC]:08x}')
         return '\n'.join(res)
 
@@ -124,13 +130,13 @@ def get_bits(dat, s, e):
 def sign_extend(x, b):
     if not x >> (b - 1):
         return x
-    return ((1 << b) - x)
+    return -((1 << b) - x)
 
 def step():
     # fetch
     ins = r32(regfile.pc)
     opcode = Ops(get_bits(ins, 6, 0))
-    print(f'{hex(regfile.pc)} \t {opcode:>10} \t {hex(ins):>10}')
+    print(f'{hex(regfile.pc)}  {hex(ins):>10}  <{opcode}: {opcode.value}>')
         
     match opcode:
         case Ops.JAL:
@@ -142,6 +148,7 @@ def step():
                 (get_bits(ins, 30, 21) << 1) |
                 (get_bits(ins, 21, 20) << 11) |
                 (get_bits(ins, 19, 12) << 12))
+            offset = sign_extend(offset, 21)
             
             regfile[rd] = regfile.pc + 4
             regfile.pc += offset
@@ -162,57 +169,136 @@ def step():
             rd = get_bits(ins, 11, 7)
             rs1 = get_bits(ins, 19, 15)
             funct3 = Funct3(get_bits(ins, 14, 12))
-            imm = get_bits(ins, 31, 20)
+            imm = sign_extend(get_bits(ins, 31, 20), 12)
 
             match funct3:
                 case Funct3.ADD:
                     regfile[rd] = regfile[rs1] + imm
                 case Funct3.SLLI:
                     regfile[rd] = regfile[rs1] << imm
+                case Funct3.SRLI:
+                    regfile[rd] = regfile[rs1] >> imm
+                case Funct3.ORI:
+                    regfile[rd] = regfile[rs1] | imm
                 case _:
                     print(regfile)
                     raise Exception(f'Unknown op for opcode: {opcode}, funct3: {funct3}')
 
+        case Ops.LUI:
+            # U-type instruction
+            rd = get_bits(ins, 11, 7)
+            imm = get_bits(ins, 31, 12)
+            regfile[rd] = imm << 12
+
         case Ops.AUIPC:
             # U-type instruction
             rd = get_bits(ins, 11, 7)
-            imm = get_bits(ins, 31, 20)
+            imm = get_bits(ins, 31, 12)
             regfile[rd] = regfile.pc + imm
 
         case Ops.SYSTEM:
+            funct3 = Funct3(get_bits(ins, 14, 12))
+            rd = get_bits(ins, 11, 7)
+            rs1 = get_bits(ins, 19, 15)
+            csr = get_bits(ins, 31, 20)
+            match funct3:
+                case Funct3.CSRRS:
+                    print('CSRRS', rd, rs1, csr)
+                case Funct3.CSRRW:
+                    print('CSRRW', rd, rs1, csr)
+                    if csr == 3072:
+                        print('SUCCESS')
+                        return False
+                case Funct3.CSRRWI:
+                    print('CSRRWI', rd, rs1, csr)
+                case Funct3.ECALL:
+                    print('ECALL', rd, rs1, csr)
+                    if regfile[3] == 21:
+                        raise Exception('FAILURE')
+                case _:
+                    print(regfile)
+                    raise Exception(f'Unknown op for opcode: {opcode}, funct3: {funct3}')
+
+        case Ops.MISC:
             pass
+
+        case Ops.BRANCH:
+            # B-type instruction
+            rs1 = get_bits(ins, 19, 15)
+            rs2 = get_bits(ins, 24, 20)
+            funct3 = Funct3(get_bits(ins, 14, 12))
+            offset = (get_bits(ins, 32, 31) << 12 |
+                (get_bits(ins, 30, 25) << 5) |
+                (get_bits(ins, 11, 8) << 1) |
+                (get_bits(ins, 8, 7) << 11))
+            offset = sign_extend(offset, 13)
+
+            cond = False
+            match funct3:
+                case Funct3.BEQ:
+                    cond = (regfile[rs1] == regfile[rs2])
+                case Funct3.BLT:
+                    cond = (regfile[rs1] < regfile[rs2])
+                case Funct3.BGE:
+                    cond = (regfile[rs1] >= regfile[rs2])
+                case Funct3.BNE:
+                    cond = (regfile[rs1] != regfile[rs2])
+                case _:
+                    print(regfile)
+                    raise Exception(f'Unknown op for opcode: {opcode}, funct3: {funct3}')
+            if cond:
+                regfile.pc += offset
+                return True
 
         case Ops.OP:
             # R-type instruction
             rd = get_bits(ins, 11, 7)
-            imm = get_bits(ins, 31, 20)
+            imm = sign_extend(get_bits(ins, 31, 20), 12)
             rs1 = get_bits(ins, 19, 15)
-            rs1 = get_bits(ins, 24, 20)
+            rs2 = get_bits(ins, 24, 20)
             funct3 = Funct3(get_bits(ins, 14, 12))
             funct7 = get_bits(ins, 31, 25)
             match funct3:
                 case Funct3.ADD:
                     regfile[rd] = regfile[rs1] + imm
+                case Funct3.OR:
+                    regfile[rd] = regfile[rs1] | imm
                 case _:
                     print(regfile)
                     raise Exception(f'Unknown op for opcode: {opcode}, funct3: {funct3}')
+
+        case Ops.STORE:
+            # S-type instruction
+            rs1 = get_bits(ins, 19, 15)
+            rs2 = get_bits(ins, 24, 20)
+            width = get_bits(ins, 14, 12)
+            offset = sign_extend((get_bits(ins, 31, 25) << 5) | get_bits(ins, 11, 7), 12)
+
+            addr = regfile[rs1] + offset
+            dat = regfile[rs2]
+
+            print('STORE: ', hex(addr), dat)
+
+        case Ops.LOAD:
+            # I-type instruction
+            rd = get_bits(ins, 11, 7)
+            rs1 = get_bits(ins, 19, 15)
+            funct3 = Funct3(get_bits(ins, 14, 12))
+            imm = sign_extend(get_bits(ins, 31, 20), 12)
+
+            addr = regfile[rs1] + imm
+            print('LOAD: ', hex(addr))
 
         case _:
             print(regfile)
             raise Exception(f'Unknown opcode: {opcode}')
 
-    # decode
-
-    # execute
-
-    # access
-
-    # write-back
+    print(regfile)
     regfile.pc  += 4
     return True
 
 if __name__ == '__main__':
-    for x in glob.glob('tests/isa/rv32ui-*'):
+    for x in glob.glob('tests/isa/rv32ui-p-add*'):
         if x.endswith('.dump'):
             continue
 
